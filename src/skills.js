29d0d084a365ae2c511f03addd0867244f95fff5
zainab7681051 <zainab76810510@gmail.com>
2024-07-13 00:47:13 +0300

    updated skills.js

--- src/skills.js
+++ src/skills.js
@@ -1,5 +1,5 @@
 export const skills = {
-    frontend: ["html", "css", "javascript", "typescript", "vue", "tailwind css", "sass", "babel", "webpack"],
-    backend: ["node js", "express js", "c#", ".net core", "C/C++", "pyhton", "java", "sql"],
+    frontend: ["html", "css", "javascript", "typescript", "vue", "tailwind css", "sass", "babel", "webpack","CORS", "Axios", "HTTP interception/transformation", "Localization/Internationalization", "pagination", "client-side storage mechanisms"],
+    backend: ["node js", "express js", "c#", ".net core", "C/C++", "pyhton", "java", "sql relational database", "no-sql database", "object relational mapping"],
     other: ["bash", "powershell", "git", "testing", "docker"]
 };
