608c4a589a668927ae603ce3d06c3a597354b99a
zainab7681051 <zainab76810510@gmail.com>
2024-07-15 14:03:40 +0300

    modified skills.js

--- src/projects.js
+++ src/projects.js
@@ -15,13 +15,6 @@ export const projects = [
         "appUrl": "https://fileconvertertool.web.app"
     },
     {
-        "title": "Invidious Instances",
-        "description": "App for fetching the free and open-source Invidious instances using the invidious API",
-        "imgSrc": "Invidious-Instances.png",
-        "githubUrl": "https://github.com/zainab7681051/get-invidious-instances",
-        "appUrl": "https://zainab7681051.github.io/get-invidious-instances/"
-    },
-    {
         "title": "DisneyMoviesWatchlist",
         "description": "DisneyMoviesWatchlist is a server-side web application in C# - ASP.NET Core, Entity Framework and Razor Pages",
         "imgSrc": "DisneyMoviesWatchlist.png",
@@ -36,18 +29,11 @@ export const projects = [
         "appUrl": "https://apidisneymovies.bsite.net/api/v1/movies/all"
     },
     {
-        "title": "PacMan-Game",
-        "description": "A basic PacMan game in vanilla Javascript and HTML Canvas Element.",
-        "imgSrc": "pacman.png",
-        "githubUrl": "https://github.com/zainab7681051/basic-pac-man-game",
-        "appUrl": "https://basic-pacman-game.web.app/"
-    },
-    {
-        "title": "Image-Genreator",
-        "description": "A node express app for generating input-based images using openAI technology.",
-        "imgSrc": "open-ai-image-generator-api.png",
-        "githubUrl": "https://github.com/zainab7681051/image-generation-app-with-openAI/",
-        "appUrl": ""
+        "title": "Invidious Instances",
+        "description": "App for fetching the free and open-source Invidious instances using the invidious API",
+        "imgSrc": "Invidious-Instances.png",
+        "githubUrl": "https://github.com/zainab7681051/get-invidious-instances",
+        "appUrl": "https://zainab7681051.github.io/get-invidious-instances/"
     },
     {
         "title": "Pizza-Delivery",
@@ -57,17 +43,31 @@ export const projects = [
         "appUrl": "https://pizza-delivery-5201d.web.app/"
     },
     {
-        "title": "Free-Documentaries",
-        "description": "A web App for viewing youtube documentaries",
-        "imgSrc": "free-documentaries.png",
-        "githubUrl": "https://github.com/zainab7681051/free-documentaries",
-        "appUrl": ""
+        "title": "PacMan-Game",
+        "description": "A basic PacMan game in vanilla Javascript and HTML Canvas Element.",
+        "imgSrc": "pacman.png",
+        "githubUrl": "https://github.com/zainab7681051/basic-pac-man-game",
+        "appUrl": "https://basic-pacman-game.web.app/"
     },
-    {
-        "title": "todo app",
-        "description": "simple todo app for keepig track of your tasks and objectives.",
-        "imgSrc": "todolist.png",
-        "githubUrl": "https://github.com/zainab7681051/simple-to-do-list-app",
-        "appUrl": ""
-    }
+    // {
+    //     "title": "Image-Genreator",
+    //     "description": "A node express app for generating input-based images using openAI technology.",
+    //     "imgSrc": "open-ai-image-generator-api.png",
+    //     "githubUrl": "https://github.com/zainab7681051/image-generation-app-with-openAI/",
+    //     "appUrl": ""
+    // },
+    // {
+    //     "title": "Free-Documentaries",
+    //     "description": "A web App for viewing youtube documentaries",
+    //     "imgSrc": "free-documentaries.png",
+    //     "githubUrl": "https://github.com/zainab7681051/free-documentaries",
+    //     "appUrl": ""
+    // },
+    // {
+    //     "title": "todo app",
+    //     "description": "simple todo app for keepig track of your tasks and objectives.",
+    //     "imgSrc": "todolist.png",
+    //     "githubUrl": "https://github.com/zainab7681051/simple-to-do-list-app",
+    //     "appUrl": ""
+    // }
 ]
